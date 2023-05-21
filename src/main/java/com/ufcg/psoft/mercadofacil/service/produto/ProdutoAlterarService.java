ae7ba2ccd3dd53c9e51ce55a977af1bc99afd429
VimersonSilva <vimerson.silva@ccc.ufcg.edu.br>
2023-05-21 19:54:09 -0300

    Refatoramento para nome Produto [Produto -> Sabor]

--- src/main/java/com/ufcg/psoft/mercadofacil/service/produto/ProdutoAlterarService.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.psoft.mercadofacil.service.produto;
-
-import com.ufcg.psoft.mercadofacil.dto.produto.ProdutoPostPutRequestDTO;
-import com.ufcg.psoft.mercadofacil.model.Produto;
-
-@FunctionalInterface
-public interface ProdutoAlterarService {
-    Produto alterar(Long id, ProdutoPostPutRequestDTO produtoPostPutRequestDTO);
-}
