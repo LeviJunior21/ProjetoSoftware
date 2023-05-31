90c11a2422e1b792206651d69d881af49c8db9f7
Victor <victorsv272011@gmail.com>
2023-05-30 22:03:57 -0300

    Deletando Services desnecessários; Condições e detalhes da solicitação de pedido pelo cliente concluidos; Testes adicionais criados para métodos de pagamento em ClienteV1ControllerTests

--- src/main/java/com/ufcg/psoft/mercadofacil/service/cliente/ClienteSolicitarPedidoPadraoService.java
+++ src/main/java/com/ufcg/psoft/mercadofacil/service/cliente/ClienteSolicitarPedidoPadraoService.java
@@ -1,16 +1,15 @@
 package com.ufcg.psoft.mercadofacil.service.cliente;
 
+import com.ufcg.psoft.mercadofacil.dto.calculadora.CalculaPrecoPizzas;
 import com.ufcg.psoft.mercadofacil.dto.cliente.ClientePedidoRequestDTO;
 import com.ufcg.psoft.mercadofacil.dto.estabelecimento.EstabelecimentoDTO;
-import com.ufcg.psoft.mercadofacil.exception.ClienteNaoExisteException;
-import com.ufcg.psoft.mercadofacil.exception.CodigoAcessoDiferenteException;
-import com.ufcg.psoft.mercadofacil.exception.EstabelecimentoNaoExisteException;
-import com.ufcg.psoft.mercadofacil.exception.PizzaNaoEncontradaException;
+import com.ufcg.psoft.mercadofacil.exception.*;
 import com.ufcg.psoft.mercadofacil.model.Cliente;
 import com.ufcg.psoft.mercadofacil.model.Estabelecimento;
 import com.ufcg.psoft.mercadofacil.repository.ClienteRepository;
 import com.ufcg.psoft.mercadofacil.repository.EstabelecimentoRepository;
 import com.ufcg.psoft.mercadofacil.service.cliente.ClienteSolicitarPedidoService;
+import com.ufcg.psoft.mercadofacil.service.estabelecimento.pagamento.PagamentoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +27,9 @@ public class ClienteSolicitarPedidoPadraoService implements ClienteSolicitarPedi
 
     @Autowired
     ModelMapper modelMapper;
+    CalculaPrecoPizzas calculadora = new CalculaPrecoPizzas();
+
+    PagamentoService pagamentoService = new PagamentoService();
 
     @Override
     public EstabelecimentoDTO solicitar(Long idCliente, Long idEstabelecimento, ClientePedidoRequestDTO clientePedidoRequestDTO) {
@@ -39,6 +41,23 @@ public class ClienteSolicitarPedidoPadraoService implements ClienteSolicitarPedi
             throw new PizzaNaoEncontradaException();
         }
         Estabelecimento estabelecimento = estabelecimentoRepository.findById(idEstabelecimento).orElseThrow(EstabelecimentoNaoExisteException::new);
+        if(clientePedidoRequestDTO == null || clientePedidoRequestDTO.equals("")){
+            clientePedidoRequestDTO.setEndereco(cliente.getEnderecoPrincipal());
+        }
+        Double valorTotal = calculadora.calculaPrecoPizzas(clientePedidoRequestDTO.getCarrinho().getPizzas());
+        if(clientePedidoRequestDTO.getMetodoPagamento().equals("PIX")){
+            valorTotal = pagamentoService.pagamentoPix(valorTotal);
+        }
+        else if(clientePedidoRequestDTO.getMetodoPagamento().equals("DEBITO")){
+            valorTotal = pagamentoService.pagamentoDebito(valorTotal);
+        }
+        else if(clientePedidoRequestDTO.getMetodoPagamento().equals("CREDITO")){
+            valorTotal = pagamentoService.pagamentoCredito(valorTotal);
+        }
+        else{
+            throw new MetodoPagamentoException();
+        }
+        clientePedidoRequestDTO.getCarrinho().setValorPedido(valorTotal);
         estabelecimento.getPedidos().add(clientePedidoRequestDTO.getCarrinho());
         Estabelecimento estabelecimento1 = estabelecimentoRepository.save(estabelecimento);
         EstabelecimentoDTO estabelecimentoDTO = modelMapper.map(estabelecimento1, EstabelecimentoDTO.class);
--- src/main/java/com/ufcg/psoft/mercadofacil/service/pedido/PedidoAlterarValorPadraoService.java
+++
@@ -1,4 +0,0 @@
-package com.ufcg.psoft.mercadofacil.service.pedido;
-
-public class PedidoAlterarValorPadraoService implements PedidoAlterarValorService{
-}
